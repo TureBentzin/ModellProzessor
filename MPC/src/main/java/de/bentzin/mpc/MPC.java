599ebfb841b859c22f3fdab4b82178166ce51af8
Ture Bentzin <38729194+TureBentzin@users.noreply.github.com>
2024-01-18 11:19:34 +0100

    migrate MPC to java

---
+++ MPC/src/main/java/de/bentzin/mpc/MPC.java
@@ -0,0 +1,14 @@
+package de.bentzin.mpc;
+
+import java.util.logging.Logger;
+
+public class MPC {
+
+    @NotNull
+    public static Logger logger = Logger.getLogger(MPC.class.getName());
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
