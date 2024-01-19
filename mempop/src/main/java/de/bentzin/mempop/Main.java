6dbe4d675958dd29cc97cf6235f1307b6f1f509b
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2024-01-19 17:01:12 +0100

    create mempop

---
+++ mempop/src/main/java/de/bentzin/mempop/Main.java
@@ -0,0 +1,12 @@
+package de.bentzin.mempop;
+
+
+/**
+ * @author Ture Bentzin
+ * @since 19-01-2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
