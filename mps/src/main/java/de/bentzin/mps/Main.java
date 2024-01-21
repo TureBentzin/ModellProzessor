50423bfb34b412c6016b16f73546b8a8f908701e
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2024-01-21 11:32:42 +0100

    create mps

---
+++ mps/src/main/java/de/bentzin/mps/Main.java
@@ -0,0 +1,12 @@
+package de.bentzin.mps;
+
+
+/**
+ * @author Ture Bentzin
+ * @since 21-01-2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
