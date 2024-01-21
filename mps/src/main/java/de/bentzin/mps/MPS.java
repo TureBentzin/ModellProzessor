5553529db45d1ff79d5ebd067ad1fe928d99c842
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2024-01-21 11:33:41 +0100

    add manifest config for mps

--- mps/src/main/java/de/bentzin/mps/MPS.java
+++ mps/src/main/java/de/bentzin/mps/MPS.java
@@ -5,7 +5,7 @@ package de.bentzin.mps;
  * @author Ture Bentzin
  * @since 21-01-2024
  */
-public class Main {
+public class MPS {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
